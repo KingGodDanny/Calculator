56b6a57f8c50be1f09efc00f4139b5fd275ff960
Danny <ctct25@naver.com>
2021-08-18 16:54:24 +0900

    Android - Calculator

--- app/src/main/java/com/javaex/calculator/MainActivity.java
+++ app/src/main/java/com/javaex/calculator/MainActivity.java
@@ -39,12 +39,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                String viewText = edtText.getText().toString();
 
                 switch (view.getId()) {
                     case R.id.one :
                         Log.d("calculator", "1 작동" + R.id.one);
 
-                        edtText.setText(one.getText().toString());
+                        edtText.setText(viewText+ 1);
 
 
                         break;
@@ -52,90 +53,91 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.two :
                         Log.d("calculator", "2 작동" + R.id.two);
 
-                        edtText.setText(two.getText().toString());
+                        edtText.setText(viewText+ 2);
 
                         break;
 
                     case R.id.three :
                         Log.d("calculator", "3 작동" + R.id.three);
 
-                        edtText.setText(three.getText().toString());
+                        edtText.setText(viewText+ 3);
 
                         break;
 
                     case R.id.four :
                         Log.d("calculator", "4 작동" + R.id.four);
 
-                        edtText.setText(four.getText().toString());
+                        edtText.setText(viewText+ 4);
 
                         break;
 
                     case R.id.five :
                         Log.d("calculator", "5 작동" + R.id.five);
 
-                        edtText.setText(five.getText().toString());
+                        edtText.setText(viewText+ 5);
 
                         break;
 
                     case R.id.six :
                         Log.d("calculator", "6 작동" + R.id.six);
 
-                        edtText.setText(six.getText().toString());
+                        edtText.setText(viewText+ 6);
 
                         break;
 
                     case R.id.seven :
                         Log.d("calculator", "7 작동" + R.id.seven);
 
-                        edtText.setText(seven.getText().toString());
+                        edtText.setText(viewText+ 7);
 
                         break;
 
                     case R.id.eight :
                         Log.d("calculator", "8 작동" + R.id.eight);
 
-                        edtText.setText(eight.getText().toString());
+                        edtText.setText(viewText+ 8);
 
                         break;
 
                     case R.id.nine :
                         Log.d("calculator", "9 작동" + R.id.nine);
 
-                        edtText.setText(nine.getText().toString());
+                        edtText.setText(viewText+ 9);
 
                         break;
 
                     case R.id.zero :
                         Log.d("calculator", "0 작동" + R.id.zero);
 
-                        edtText.setText(zero.getText().toString());
+                        edtText.setText(viewText+ 0);
 
                         break;
 
                     case R.id.slash :
                         Log.d("calculator", "/ 작동" + R.id.slash);
 
-                        edtText.setText(slash.getText().toString());
+                        edtText.setText(viewText+ "/");
 
                         break;
 
                     case R.id.multi :
                         Log.d("calculator", "X 작동" + R.id.multi);
 
-                        edtText.setText(multi.getText().toString());
+                        edtText.setText(viewText+ "*");
 
                         break;
 
                     case R.id.minus :
                         Log.d("calculator", "- 작동" + R.id.minus);
 
-                        edtText.setText(minus.getText().toString());
+                        edtText.setText(viewText+ "-");
 
                         break;
 
                     case R.id.plus :
+                        Log.d("calculator", "+ 작동" + R.id.minus);
 
-                        edtText.setText(plus.getText().toString());
+                        edtText.setText(viewText+ "+");
 
                         break;
 
@@ -148,19 +150,31 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.equal :
                         Log.d("calculator", "= 작동" + R.id.equal);
 
+                        double result = 0.0;
 
-                        /*
-                        String sign = edtText.getText().toString();
+                        if(viewText.contains("/")) {
+                            Log.d("calculator", "/ 작동");
+                            String[] str = viewText.split("/");
+                            result = Double.parseDouble(str[0]) / Double.parseDouble((str[1]));
 
-                        int result = 0;
+                        } else if(viewText.contains("*")) {
+                            Log.d("calculator", "* 작동");
+                            String[] str = viewText.split("\\*");
+                            result = Double.parseDouble(str[0]) * Double.parseDouble((str[1]));
 
-                        if(sign.contains("+")) {
-                            String[] num = (edtText.getText().toString()).split("\\+");
-                            result = Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+                        } else if(viewText.contains("-")) {
+                            Log.d("calculator", "* 작동");
+                            String[] str = viewText.split("-");
+                            result = Double.parseDouble(str[0]) - Double.parseDouble((str[1]));
+
+                        } else if(viewText.contains("+")) {
+                            Log.d("calculator", "+ 작동");
+                            String[] str = viewText.split("\\+");
+                            result = Double.parseDouble(str[0]) + Double.parseDouble((str[1]));
                         }
 
-                        edtText.setText(equal.getText().toString()+ result);
-                        */
+                        edtText.setText(equal.getText().toString() + result);
+
                         break;
 
 
